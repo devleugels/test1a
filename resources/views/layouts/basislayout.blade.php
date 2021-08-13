f29e832be9fe03495d1ef264b373dc9afcfe1f6e
calu <johan.calu@gmail.com>
2021-08-13 10:13:31 +0200

    npm en tailwindcss installeren

---
+++ resources/views/layouts/basislayout.blade.php
@@ -0,0 +1,10 @@
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<!-- CSRF token -->
+@csrf 
+
+<title>{{ config('app.name', 'Vleugels') }}</title>
+ 
+<link rel="preconnect" href="https://fonts.gstatic.com">
+<link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;600;700&display=swap" rel="stylesheet">
+<script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.x.x/dist/alpine.min.js" defer></script>
